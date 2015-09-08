36dd59535756e2b48e32efcf514b13c18225c23d
DiaaMohsen <diemma2015@yahoo.com>
2015-09-08 14:37:13 +0200

    add permissions in my own auth_sign, let's try sign_req again

--- V0/urls.py
+++ V0/urls.py
@@ -4,18 +4,18 @@ from . import views
 
 urlpatterns = [
 
-
- # ex: /polls/5/vote/
-#    url(r'^(?<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-
-
-#	url(r'^login/$', views.login, name='login'),
 	url(r'^$', views.login, name='login'),
 	url(r'^auth_login$', views.auth_login, name='auth_login'),
 	url(r'^logout$', views.logout, name='logout'),
 	url(r'^home$', views.home, name='home'),
 	url(r'^invalid_login$', views.invalid_login, name='invalid_login'),
 
+
+	url(r'^sign_up$', views.sign_up, name='sign_up'),
+	url(r'^auth_signup$', views.auth_signup, name='auth_signup'),
+#	url(r'^invalid_signup$', views.invalid_signup, name='invalid_signup'),
+	url(r'^signup_success$', views.signup_success, name='signup_success'),
+	url(r'^signup_req$', views.signup_req, name='signup_req'),
 
 ]
 '''
--- V0/views.py
+++ V0/views.py
@@ -2,12 +2,16 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render, render_to_response
 from django.http import Http404
 from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
 from django.views import generic
 from .models import Task, Owner
 
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 
 #def index(request):
 #	return render(request, 'V0/login.html')
@@ -17,13 +21,7 @@ from .models import Task, Owner
 #    context = {'tasks_list': tasks_list}
 #    return render(request, 'V0/index.html')#, context)
 
-'''
-	url(r'^$', views.login, name='login'),
-	url(r'^auth$', views.auth_login),
-	url(r'^logout$', views.logout, name='logout'),
-	url(r'^loggedin$', views.loggedin, name='home'),
-	url(r'^invalid_login$', views.invalid_login),
-'''
+
 def login(request):
 	c = {}
 	c.update(csrf(request))
@@ -37,18 +35,19 @@ def auth_login(request):
 #	return HttpResponse(user.username)
 	if user :
 		auth.login(request, user)
-		print "HIiiiiiiiiiiiiiii"
 #		return HttpResponseRedirect('V0:home')
 		return HttpResponseRedirect(reverse('V0:home'))
 
 	else:
-#		return HttpResponse('WTF')
 		return HttpResponseRedirect(reverse('V0:invalid_login'))
 #		return HttpResponseRedirect('V0:invalid_login')
 
 
 def home(request):
-	return render_to_response('V0/home.html', {'uname': request.user.username})
+	user = User.objects.get(username=request.user.username)
+	context = {'user': user}
+#	return render(request, 'V0/show_tasks.html', context)	
+	return render_to_response('V0/home.html', context)#{'username': request.user.username})
 
 def invalid_login(request):
 	return render_to_response('V0/invalid_login.html')#, {'message': 'username or password not correct'})
@@ -56,3 +55,57 @@ def invalid_login(request):
 def logout(request):
 	auth.logout(request)
 	return render_to_response('V0/logout.html')
+
+def sign_up(request):
+	c = {}
+	c.update(csrf(request))
+	return render_to_response('V0/signup.html', c)
+
+def signup_req(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('V0:signup_success'))
+		else:
+			print form.cleaned_data['username']
+	args = {}
+	args.update(csrf(request))
+	args['form'] = UserCreationForm()
+#	return HttpResponse('this username is exist already')
+	return render_to_response('V0/signup.html', args)
+
+def signup_success(request):
+	return render_to_response('V0/signup_success.html')
+
+
+def invalid_signup(request): # to be done later
+	return HttpResponse('this username is exist already')
+
+
+
+
+def auth_signup(request):
+	
+	uname = request.POST['username']
+	pwrd1 = request.POST['password1']
+	pwrd2 = request.POST['password2']
+
+	user = User.objects.filter(username=uname).exists()
+
+
+	if user:# b3den 5liha view l-w7dha
+#		return HttpResponseRedirect(reverse(V0:invalid_signup))
+		return HttpResponse('this username is exist already') 
+
+	else:
+		if pwrd1 != pwrd2:
+			return HttpResponse('rewrite passwords fields again')
+
+	nuser = User.objects.create_user(uname, pwrd1)
+	nuser.user_permissions = [	Permission.objects.get(codename='add_task'),
+								Permission.objects.get(codename='change_task'),
+								Permission.objects.get(codename='delete_task')
+							]
+	nuser.save()
+	return HttpResponseRedirect(reverse('V0:login'))
