bcfe1e798909345b18a6136b8dfee2a6c345e364
DiaaMohsen <diemma2015@yahoo.com>
2015-09-08 11:48:46 +0200

    add auth for login

--- V0/urls.py
+++ V0/urls.py
@@ -11,11 +11,17 @@ urlpatterns = [
 
 #	url(r'^login/$', views.login, name='login'),
 	url(r'^$', views.login, name='login'),
+	url(r'^auth_login$', views.auth_login, name='auth_login'),
+	url(r'^logout$', views.logout, name='logout'),
+	url(r'^home$', views.home, name='home'),
+	url(r'^invalid_login$', views.invalid_login, name='invalid_login'),
+
+
+]
+'''
     url(r'^show_tasks/$', views.show_tasks, name='show_tasks'),
 	url(r'^signup/$', views.signup, name='signup'),
 	url(r'^validate_signup/$', views.validate_signup, name='validate_signup'),
 	url(r'^validate_login/$', views.validate_login, name='validate_login'),
 	#url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-
-
-]
+'''
--- V0/views.py
+++ V0/views.py
@@ -1,12 +1,12 @@
-from django.http import HttpResponseRedirect,HttpResponse
-from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render, render_to_response
 from django.http import Http404
+from django.contrib import auth
+from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
 from django.views import generic
 from .models import Task, Owner
-from django.utils import timezone
-from django.contrib import messages
 
 
 #def index(request):
@@ -17,66 +17,42 @@ from django.contrib import messages
 #    context = {'tasks_list': tasks_list}
 #    return render(request, 'V0/index.html')#, context)
 
-
-def signup(request):
-	return render(request, 'V0/signup.html')
-
-
-def validate_signup(request): # 34an asht3'l bs dlwa2ti
-	#uname = request.POST['username']
-	if Owner.objects.filter(uname = request.POST['username']):
-	#	messages.warning(request, 'Your account expires in three days.')
-	#	messages.add_message(request, messages.INFO, "Existed Username")
-		return HttpResponseRedirect(reverse('V0:signup'))
-	else:
-		return HttpResponseRedirect(reverse('V0:login'))
-#	return render(request, 'V0/login.html')
-
+'''
+	url(r'^$', views.login, name='login'),
+	url(r'^auth$', views.auth_login),
+	url(r'^logout$', views.logout, name='logout'),
+	url(r'^loggedin$', views.loggedin, name='home'),
+	url(r'^invalid_login$', views.invalid_login),
+'''
 def login(request):
-	return render(request, 'V0/login.html')
-#	return HttpResponse('LOGIN?')
+	c = {}
+	c.update(csrf(request))
+	return render_to_response('V0/login.html',c)
 
-def validate_login(request):
-#	if Owner.objects.filter(uname = request.POST['username']):	# login
-#	else: arert w 5lik mkank
-#	return HttpResponse("here")
-	if request.POST:
-		uname = request.POST["username"]
-	#	return HttpResponse(reverse('V0:show_tasks', args=(uname,)))
-		return HttpResponse(show_tasks(request,uname))
+
+def auth_login(request):
+	uname = request.POST['username']
+	pword = request.POST['password']
+	user = auth.authenticate(username=uname, password=pword)
+#	return HttpResponse(user.username)
+	if user :
+		auth.login(request, user)
+		print "HIiiiiiiiiiiiiiii"
+#		return HttpResponseRedirect('V0:home')
+		return HttpResponseRedirect(reverse('V0:home'))
+
 	else:
-		return HttpResponse("hello")
-		
-#	return HttpResponseRedirect(reverse('V0:show_tasks', args=(uname)))
+#		return HttpResponse('WTF')
+		return HttpResponseRedirect(reverse('V0:invalid_login'))
+#		return HttpResponseRedirect('V0:invalid_login')
 
-def show_tasks(request,u_name):
 
-#	id_ = Owner.objects.get(uname=str(request.POST.get("username",""))).pk
-#	tasks_list = Task.objects.filter(owner_id=id_)#request.POST.get("username",""))
-#	context = {'tasks_list': tasks_list}
+def home(request):
+	return render_to_response('V0/home.html', {'uname': request.user.username})
 
-	ownr = Owner.objects.get(uname=u_name)
-	context = {'ownr': ownr}
-	return render(request, 'V0/show_tasks.html', context)
-#	return HttpResponse("ads")
+def invalid_login(request):
+	return render_to_response('V0/invalid_login.html')#, {'message': 'username or password not correct'})
 
-'''
-    context = {'tasks_list': tasks_list}
-    return render(request, 'V0/show_tasks.html', context)
-'''
-#	return render(request, 'V0/show_tasks.html')
-'''
-    tasks_list = Task.objects.all()#order_by('-pub_date')[:5]
-    context = {'tasks_list': tasks_list}
-
-    return render(request, 'V0/index.html')#, context)
-'''
-
-#def validate(request):
-
-''''
-def index(request):
-    owner_tasks_list = Task.objects.get(pk=owner_id)#order_by('-pub_date')[:5]
-    context = {'owner_tasks_list': owner_tasks_list}
-    return render(request, 'V0/index.html', context)
-'''
+def logout(request):
+	auth.logout(request)
+	return render_to_response('V0/logout.html')
